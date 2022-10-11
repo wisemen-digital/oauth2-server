2d776d084b728b1bcaa7570499cef152f6f32e3e
Joren Vandeweyer <joren.vandeweyer@gmail.com>
2022-10-11 17:17:01 +0200

    0.0.4 release

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
+import OAuth2Server from '@node-oauth/oauth2-server'
+
 export * from './oauth2'
 export * from './types'
 
-import OAuth2Server from '@node-oauth/oauth2-server'
+export { Token, Request, Response } from '@node-oauth/oauth2-server'
 
 export { OAuth2Server }
