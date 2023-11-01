9ad2aff17613f019db95e58d56973bb98d0f47de
Joren Vandeweyer <joren.vandeweyer@appwise.be>
2023-11-01 17:11:53 +0100

    export extra error types

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ export { DefaultGrantType } from './grants/DefaultGrantType'
 export * from './oauth2'
 export * from './types'
 
-export { Request, Response, AbstractGrantType, InvalidArgumentError } from '@node-oauth/oauth2-server'
+export { Request, Response, AbstractGrantType, InvalidArgumentError, InvalidGrantError, InvalidScopeError } from '@node-oauth/oauth2-server'
 
 export { OAuth2Server }
 
