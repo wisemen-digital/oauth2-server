3a0aab166bf7b5de95b3acaffa17fc8e32c33c64
siemenvdn <siemen.vandenneste@wisemen.digital>
2024-01-31 09:50:11 +0100

    verificationcode is string

--- src/types.ts
+++ src/types.ts
@@ -65,7 +65,7 @@ export interface CodeService {
 }
 
 export interface VerificationCodeService {
-  verify: (key: string, code: number) => Promise<User | false>
+  verify: (key: string, code: string) => Promise<User | false>
 }
 
 export interface PKCEService {
